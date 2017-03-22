2ec809daf03b52a274f109b473cfdb9350012f66
Richard Wiley <binky@server.local>
2017-03-22 14:20:44 -0400

    Update database entries in wp-config.php file

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress_db');
+define('DB_NAME', 'heroku_0c8c347c25d44a3');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'b13f31dfa439d3');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'ie0ed1d49');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
